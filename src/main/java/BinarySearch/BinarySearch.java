01e49c104257d0307939ef994eaf066cfe98a3bc
Katerina <ekate-meshkova@yandex.ru>
2025-02-12 23:09:27 +0300

    Changes

--- src/main/java/BinarySearch/BinarySearch.java
+++ src/main/java/BinarySearch/BinarySearch.java
@@ -1,31 +1,39 @@
 package BinarySearch;
 
+import org.example.models.Bus;
+import org.example.sorting.SelectionSort;
+
 public class BinarySearch {
 
-    public static int rank (int key, int [ ] a) {
+    public static <T> int rank  (T key, T [ ] a, Comparator <T>  comparator) {
         int min = 0;
         int max = a.length - 1;
         int count = 0 ;
         while (min <= max) {
             int mid = ((max - min) / 2) + min;
-            if (key > a [mid])
-                min = mid -1;
-            else if (key ==a[mid]) {
+            if (comparator.compare(key, a [mid]) > 0)
+                min = mid + 1;
+            else if (comparator.compare (key, a[mid]) < 0)
+                 max = mid - 1;
+            else if (comparator.compare (key, a[mid] )==0) {
                 return mid;
             }
             count ++;
         }
         return -1;
     }
-    public static void main (String [] args) {
-        int data [ ] = { 3, 6, 7, 10, 34, 56, 60 } ;
-        int numberToFind = 10;
-        int answer  = rank (numberToFind, data);
-        if (answer >= 0) {
-            System.out.println( answer);
-        } else {
-            System.out.println("Не найдено");
+    public static void main (String [] args) { Bus[ ] buses = {
+                new Bus.BusBuilder() .setNumber(123).setModel("Volvo"). setMileage(500000).build(),
+            new Bus.BusBuilder().setNumber(789).setModel("Mercedes").setMileage(30000).build(),
+             new Bus.BusBuilder().setNumber(456).setModel("Iveco"). setMileage(15000).build(),
+            new Bus.BusBuilder() .setNumber(749).setModel("Mercedes"). setMileage(300000).build(),
+            new Bus.BusBuilder() .setNumber(366).setModel("Iveco"). setMileage(15000).build()};
+
+            SelectionSort<Bus>sorter =new SelectionSort<>();
+                    sorter.sort(buses, new Bus.BusNumberComparator());
+        int result =BinarySearch.rank (new Bus.BusBuilder().setNumber(456).build(), buses,new Bus.BusNumberComparator());
+        System.out.println(buses[result]);
         }
     }
-}
+
 
